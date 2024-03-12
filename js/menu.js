a56298d37d10a845f66cf16930e6a7ad3a5a2e36
profecf <fbaz@xtec.cat>
2024-03-12 13:26:22 +0100

    Afegir menú desplegable i estils associats

---
+++ js/menu.js
@@ -0,0 +1,44 @@
+window.onload = function () {
+
+    var menuItems = document.querySelectorAll('li.has-submenu');
+
+    Array.prototype.forEach.call(menuItems, function (el, i) {
+
+        var timer;
+
+        /* Afegir botó per desplegar menú */
+        var activatingA = el.querySelector('a');
+        var btn = '<button><span><span class="visuallyhidden">mostrar submenú de “' 
+                + activatingA.text 
+                + '”</span></span></button>';
+	    activatingA.insertAdjacentHTML('afterend', btn);
+
+        /* Control d'estats del desplegable */
+        el.querySelector('button').addEventListener("click",  function(event){
+            if (this.parentNode.className == "has-submenu") {
+                this.parentNode.className = "has-submenu open";
+                this.parentNode.querySelector('a').setAttribute('aria-expanded', "true");
+                this.parentNode.querySelector('button').setAttribute('aria-expanded', "true");
+            } else {
+                this.parentNode.className = "has-submenu";
+                this.parentNode.querySelector('a').setAttribute('aria-expanded', "false");
+                this.parentNode.querySelector('button').setAttribute('aria-expanded', "false");
+            }
+            event.preventDefault();
+        });
+
+        /* Events per obrir/tancar desplegable */
+        el.addEventListener("mouseover", function (event) {
+            this.className = "has-submenu open";
+            clearTimeout(timer);
+        });
+
+        el.addEventListener("mouseout", function (event) {
+            /* tancar amb retard per usuaris amb discapacitat motriu */
+            timer = setTimeout(function (event) {
+                document.querySelector(".has-submenu.open").className = "has-submenu";
+            }, 1000);
+        });
+
+    });
+}
