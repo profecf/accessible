0ee50b485ae76cb50c512a6420fc35b6af434795
EdwinAMV <edalbert1407@gmail.com>
2024-03-19 20:54:18 +0100

    Afegir modal amb script i estil

---
+++ js/modal.js
@@ -0,0 +1,23 @@
+// modal.js
+
+// Obtén el botón de abrir el modal y el modal
+var modalBtn = document.getElementById("openModalBtn");
+var modal = document.getElementById("myModal");
+
+// Cuando se haga clic en el botón, muestra el modal
+modalBtn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// Cuando se haga clic en la "x" dentro del modal, ciérralo
+var closeModal = document.getElementsByClassName("close")[0];
+closeModal.onclick = function() {
+    modal.style.display = "none";
+}
+
+// Cuando el usuario haga clic en cualquier parte fuera del modal, ciérralo
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
