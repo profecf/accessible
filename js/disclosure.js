5ce8a2147719f6ba0d2a69ef31ba4517972f6cf9
Cosmin Calin <cosmin.calin22@gmail.com>
2024-03-21 17:31:46 +0100

    fixed spaces for tabs correctly formated

--- js/disclosure.js
+++ js/disclosure.js
@@ -1,20 +1,19 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Encuentra todos los botones dentro del main que controlan la visibilidad
-    var buttons = document.querySelectorAll('main section > button[aria-expanded][aria-controls]');
-  
-    buttons.forEach(function(button) {
-      button.addEventListener('click', function() {
-        var expanded = this.getAttribute('aria-expanded') === 'true';
-        var controlledElement = document.getElementById(this.getAttribute('aria-controls'));
-  
-        if (expanded) {
-          this.setAttribute('aria-expanded', 'false');
-          controlledElement.classList.remove('visible');
-        } else {
-          this.setAttribute('aria-expanded', 'true');
-          controlledElement.classList.add('visible');
-        }
-      });
+document.addEventListener('DOMContentLoaded', function () {
+  // Encuentra todos los botones dentro del main que controlan la visibilidad
+  var buttons = document.querySelectorAll('main section > button[aria-expanded][aria-controls]');
+
+  buttons.forEach(function (button) {
+    button.addEventListener('click', function () {
+      var expanded = this.getAttribute('aria-expanded') === 'true';
+      var controlledElement = document.getElementById(this.getAttribute('aria-controls'));
+
+      if (expanded) {
+        this.setAttribute('aria-expanded', 'false');
+        controlledElement.classList.remove('visible');
+      } else {
+        this.setAttribute('aria-expanded', 'true');
+        controlledElement.classList.add('visible');
+      }
     });
   });
-  
+});
