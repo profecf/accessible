e54fee51b88907bd354ab37b233cbc2b1854b8e5
Daniel <144838406+DanielCalzadaRubio@users.noreply.github.com>
2024-04-02 20:36:07 +0200

    Diferents canvis solicitats als Pull Request
    S'han implementat diferents dels canvis solicitats del Pull Request

--- js/grid.js
+++ js/grid.js
@@ -1,38 +1,14 @@
-// Agafar del HTML per ID el hamburguer i el contingut del nav
-const hamburguer = document.getElementById('hamburger');
-const desplegable = document.getElementById('contingut-nav');
+const widgets = document.getElementById('desplegable-widgets');
+const contingutWidgets = document.getElementById('desplegable-contingut-widgets');
 
 let desplegat = false;
 
-// Cuan es faci click al hamburger que executi una funció on aplicará una condició on mirará el boolean i fará una funció o l'altre
-hamburguer.addEventListener('click', function() {
+widgets.addEventListener('mouseenter', function() {
     if (desplegat) {
-        desplegable.style.display = 'none';
+        contingutWidgets.style.display = 'none';
     } else {
-        desplegable.style.display = 'block';
+        contingutWidgets.style.display = 'block';
     }
 
-    // Fiquem el sentit del boolean al reves
     desplegat = !desplegat;
-})
-
-// Agafar del HTML per ID la lupa i el camp de text i el botó del formulari
-const lupa = document.getElementById('lupa');
-const text = document.getElementById('text');
-const button = document.getElementById('button');
-
-let formulariDesplegat = false;
-
-// Cuan es faci click a la lupa que executi una funció on aplicará una condició on mirará el boolean i fara una funció o l'altre
-lupa.addEventListener('click', function() {
-    if (formulariDesplegat) {
-        text.style.display = 'none';
-        button.style.display = 'none';
-    } else {
-        text.style.display = 'block';
-        button.style.display = 'block';
-    }
-
-    // Fiquem el sentit del boolean al reves
-    formulariDesplegat = !formulariDesplegat;
 })
